ff79f996df314f8fb8be9193af4b872aab1640f2
José Luis López G <jose.lopez@croeminc.com>
2023-09-17 13:43:32 -0600

    Agregar modelos para el registro y acceso del usuario

---
+++ UsersApplication/ViewModels/LoginViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UsersApplication.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "El email es obligatorio")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "La contraseña es obligatoria")]        
+        [DataType(DataType.Password)]
+        [Display(Name = "Contraseña")]
+        public string Password { get; set; }
+
+        [Display(Name = "Recordarme?")]
+        public bool RememberMe { get; set; }
+    }
+}
---
+++ UsersApplication/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Identity.Client;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace UsersApplication.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "El email es obligatorio")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "La contraseña es obligatoria")]
+        [StringLength(20, ErrorMessage = "El {0} debe tener al menos {1} caracteres", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Contraseña")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "La confirmación de contraseña es obligatoria")]
+        [Compare("Password", ErrorMessage = "La contraseña y confirmación dfe contraseña no coinciden")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirmar contraseña")]
+        public string ConfirmPassword { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [Display(Name = "Nombre completo")]
+        public string FullName { get; set; }
+
+        [Required(ErrorMessage = "La fecha de nacimiento es obligatorio")]
+        [Display(Name = "Fecha de nacimiento")]
+        [DataType(DataType.Date)]
+        public DateTime BirthDate { get; set; }
+
+        [Required(ErrorMessage = "El País es obligatorio")]
+        [Display(Name = "País")]
+        public string Country { get; set; }
+
+        [Display(Name = "Ciudad")]
+        public string City { get; set; }
+
+        [Display(Name = "Teléfono")]
+        public string PhoneNumber { get; set; }
+    }
+}
