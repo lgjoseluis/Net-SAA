ad33c69a90a61529d34c14f6707be6b3dd139903
José Luis López G <jose.lopez@croeminc.com>
2023-10-15 13:37:06 -0600

    Agregar opción para modificar perfily cambiar contraseña

---
+++ UsersApplication/Controllers/UsersController.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using UsersApplication.Data;
+using UsersApplication.Models;
+using UsersApplication.ViewModels;
+
+namespace UsersApplication.Controllers
+{
+    [Authorize]
+    public class UsersController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly ApplicationDbContext _context;
+
+        public UsersController(UserManager<IdentityUser> userManager, ApplicationDbContext context)
+        {
+            _userManager = userManager;
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult EditProfile(string id) 
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                return BadRequest();
+            }
+
+            ApplicationUser user = _context.ApplicationUsers.Find(id);
+
+            if (user is null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditProfile(ApplicationUser model)
+        {
+            if(ModelState.IsValid)
+            {
+                ApplicationUser user = await _context.ApplicationUsers.FindAsync(model.Id);
+
+                user.FullName = model.FullName;
+                user.Country = model.Country;
+                user.City = model.City;
+                user.BirthDate = model.BirthDate;
+                user.PhoneNumber = model.PhoneNumber;
+
+                await _userManager.UpdateAsync(user);
+
+                return RedirectToAction(nameof(Index), "Home");
+            }
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult ChangePassword()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ChangePassword(ChangePasswordViewModel model, string email)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityUser user = await _userManager.FindByEmailAsync(email);
+
+                if(user is null)
+                {
+                    return RedirectToAction("Error"); 
+                }
+
+                string token = await _userManager.GeneratePasswordResetTokenAsync(user);
+
+                IdentityResult result = await _userManager.ResetPasswordAsync(user, token, model.Password);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("ConfirmChangePassword");
+                }
+
+                return View(model);
+            }
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult ConfirmChangePassword()
+        {
+            return View();
+        }
+    }
+}
---
+++ UsersApplication/ViewModels/ChangePasswordViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UsersApplication.ViewModels
+{
+    public class ChangePasswordViewModel
+    {
+        [Required(ErrorMessage = "La contraseña es obligatoria")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Contraseña")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "La confirmación de contraseña es obligatoria")]
+        [Compare("Password", ErrorMessage = "La contraseña y confirmación de contraseña no coinciden")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirmar contraseña")]
+        public string ConfirmPassword { get; set; }
+    }
+}
